32e97a8f79506ce56841ad8e50a55f9050144ced
Yoriscape <36034638+yoriscape@users.noreply.github.com>
2022-09-19 22:40:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello I'm<a href="https://nextjs.org">Yoriscape!</a>
+          Hello I am <a href="https://nextjs.org">Yoriscape!</a>
         </h1>
 
         <p className={styles.description}>
